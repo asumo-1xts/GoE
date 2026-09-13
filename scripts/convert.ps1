c9c90046637ed12e5e002ffeb5094efb642ce610
aSumo <me@asumo.dev>
2026-09-13 16:52:09 +0900

    refactor: main.ahkを改名

--- scripts/convert.ps1
+++ scripts/convert.ps1
@@ -1,5 +1,5 @@
 & "$env:LOCALAPPDATA\Programs\AutoHotkey\Compiler\Ahk2Exe.exe" `
-  /in "main.ahk" `
+  /in "GoE.ahk" `
   /out "GoE.exe" `
   /icon "assets\GoE.ico" `
   /base "$env:LOCALAPPDATA\Programs\AutoHotkey\v2\AutoHotkey64.exe"
