efaab555af7f3cf98511d7d5ef71644b41d7e27f
kishimura <soma.kishimura@music.yamaha.com>
2026-09-15 10:26:04 +0900

    build: 設定を保持するファイルをビルド時に再生成

--- scripts/convert.ps1
+++ scripts/convert.ps1
@@ -1,5 +1,20 @@
-mkdir dist -Force | Out-Null
+# config.jsonを取得して、DefaultConfigJson.ahkの中身を作成
+$json = Get-Content config.json -Raw
 
+$content = @"
+#Requires AutoHotkey v2.0
+
+DefaultConfigJson := "
+(
+$json
+)"
+"@
+
+# リセットしたDefaultConfigJson.ahkファイルにコピペ
+New-Item src\DefaultConfigJson.ahk -ItemType File -Force | Out-Null
+Set-Content src\DefaultConfigJson.ahk $content -Encoding UTF8
+
+# .exeファイルを生成
 & "$env:LOCALAPPDATA\Programs\AutoHotkey\Compiler\Ahk2Exe.exe" `
   /in "GoE.ahk" `
   /out "GoE.exe" `
